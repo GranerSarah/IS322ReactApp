41c808b19e900f489d95e55f5cc665a54a4a8bcd
GranerSarah <sg698@njit.edu>
2019-04-14 04:55:57 -0400

    Taskboard Columns

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,110 +4,64 @@ import TaskItem from './TaskItem';
 
 class TaskList extends React.Component {
 
-  markDone = (task) => {
-    const taskIndex = this.props.tasks.findIndex(t => t.id === task.id);
-    let currCol=this.task.column;
+    markDone = (task) => {
+        const taskIndex = this.props.tasks.findIndex(t => t.id === task.id);
+        let taskList = this.props.tasks;
+        let currCol=task.column;
 
-    switch (currCol){
-
-      case 'todo':
-        this.task.column='in-progress';
-
-      case 'in-progress':
-        this.task.column='review';
-
-      case 'review':
-        this.task.column='done';
-
-      default:
-        this.task.column='done';
+        if(currCol==='todo'){
+            task.column="in-progress";
+        }else if(currCol==='in-progress'){
+            task.column="review";
+        }else if(currCol==='review'){
+            task.column="done";
+        }
+        this.props.onUpdateTaskList(taskList);
     }
 
-    let taskList = this.props.tasks;
-    taskList.splice(taskIndex, 1);
-    this.props.onUpdateTaskList(taskList);
-  }
+    unDone = (task) => {
+        const taskIndex = this.props.tasks.findIndex(t => t.id === task.id);
+        let taskList = this.props.tasks;
+        let currCol=task.column;
 
-  unDone = (task) => {
-    const taskIndex = this.props.tasks.findIndex(t => t.id === task.id);
-    let currCol=this.task.column;
-
-    switch (currCol){
-
-      case 'done':
-        this.task.column='review';
-
-      case 'in-progress':
-        this.task.column='todo';
-
-      case 'review':
-        this.task.column='in-progress';
-
-      default:
-        this.task.column='todo';
+        if(currCol==='review'){
+            task.column="in-progress";
+        }else if(currCol==='done'){
+            task.column="review";
+        }else if(currCol==='in-progress'){
+            task.column="todo";
+        }
+        this.props.onUpdateTaskList(taskList);
     }
-
-    let taskList = this.props.tasks;
-    taskList.splice(taskIndex, 1);
-    this.props.onUpdateTaskList(taskList);
-  }
 
 
   render() {
-    const todoList=this.props.tasks.map(
-        task=>{
-          if(task.column==='todo'){
-            return <TaskItem task={task} key={task.id} markDone={this.markDone} unDone={this.unDone}/>}
-        }
-    );
+      const todoItems = this.props.tasks.map(task => {
+          if(task.column==='todo') {
+              return <TaskItem task={task} key={task.id} markDone={this.markDone} unDone={this.unDone}/>
+          }
+      });
 
-    const progressList=this.props.tasks.map(
-        task=>{
-          if(task.column==='in-progress'){
-            return <TaskItem task={task} key={task.id} markDone={this.markDone} unDone={this.unDone}/>}
-        }
-    );
-
-    const reviewList=this.props.tasks.map(
-        task=>{
-          if(task.column==='review'){
-            return <TaskItem task={task} key={task.id} markDone={this.markDone} unDone={this.unDone}/>}
-        }
-    );
-
-    const doneList=this.props.tasks.map(
-        task=>{
-          if(task.column==='done'){
-            return <TaskItem task={task} key={task.id} markDone={this.markDone} unDone={this.unDone}/>}
-        }
-    );
+      const progressItems = this.props.tasks.map(task => {
+          if(task.column==='in-progress') {
+              return <TaskItem task={task} key={task.id} markDone={this.markDone} unDone={this.unDone}/>
+          }
+      });
 
     return(
         <section id="taskboard">
           <article id="ToDos" className="boards">
             <h2>To Do</h2>
             <ul className="task-list list-group">
-              {todoList}
+              {todoItems}
             </ul>
           </article>
-          <article id="Progressives" className="boards">
-            <h2>In Progress</h2>
-            <ul className="task-list list-group">
-              {progressList}
-            </ul>
-          </article>
-          <article id="Reviews" className="boards">
-            <h2>Review</h2>
-            <ul className="task-list list-group">
-              {reviewList}
-            </ul>
-          </article>
-          <article id="Dones" className="boards">
-            <h2>Done</h2>
-            <ul className="task-list list-group">
-              {doneList}
-            </ul>
-          </article>
+            <article id="Progresses" className="boards">
+                <h2>In Progress</h2>
+                <ul className="task-list list-group">
+                    {progressItems}
+                </ul>
+            </article>
         </section>
     );
   }
