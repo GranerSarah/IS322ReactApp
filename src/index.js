095b86129b3d37da8e052b45823a0f89dafd953c
GranerSarah <sg698@njit.edu>
2019-04-14 03:47:00 -0400

    Conditional Rendering and Page Tabs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-//Import our dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//create basic functional react component
-const App = () => {
-    return <h1>The Tasker: A Task List Example</h1>
-}
+import App from './components/App';
 
-
-//Render the react component into the DOM
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
